08cf7e6cd95ae3734982b9cd04853492788bae99
Siam05 <siam.mahmud05@gmail.com>
2022-07-07 13:24:05 +0600

    final-version

--- api-path/apipath.js
+++ api-path/apipath.js
@@ -4,13 +4,13 @@ const PAPI = {
     'get_photo': host + '/api/packages/photos',
     'get_video': host + '/api/packages/videos',
     'get_combo': host + '/api/packages/combos',
-    'get_review':host + '/api/reviews',
-    'get_image':host + '/api/images',
-    'get_film':host + '/api/videos',
-    'create_contract':host + '/api/contact/create',
-    'get_team':host + '/api/teams',
-    'create_contract':host+'/api/contact/create',
-    'get_home_banners':host+'/api/home/banners'
+    'get_review': host + '/api/reviews',
+    'get_image': host + '/api/images',
+    'get_film': host + '/api/videos',
+    'create_contract': host + '/api/contact/create',
+    'get_team': host + '/api/teams',
+    'create_contract': host + '/api/contact/create',
+    'get_home_banners': host + '/api/home/banners'
 }
 
 export default PAPI;
